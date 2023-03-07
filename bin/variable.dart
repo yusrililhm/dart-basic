d8e412942705fb8603394801f22b18533a754f72
yusrililhm <yusrililham62@gmail.com>
2023-03-07 16:52:33 +0700

    belajar variable di dart

--- bin/variable.dart
+++ bin/variable.dart
@@ -5,7 +5,24 @@ void main() {
   name = 'popoci';
   print('Hello $name');
 
-//  final
-  final human = 'Yurika';
+  // final
+  final human = ['yurika', 'laras'];
   print(human);
+
+  human[1] = 'Larasati';
+  print(human);
+
+  // const
+  const phi = 3.14;
+  print(phi * 7 * 7);
+
+  // late
+  late var value = helloName();
+  print('variable dibuat');
+  print(value);
+}
+
+String helloName() {
+  print('called function');
+  return 'helloooo';
 }
