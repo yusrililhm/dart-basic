a8cfbe51a58c40f4fdcba8683bbc56db57651c44
yusrililhm <yusrililham62@gmail.com>
2023-03-07 19:30:24 +0700

    belajar operator penugasan di dart

--- bin/assignment.dart
+++ bin/assignment.dart
@@ -1,4 +1,12 @@
 void main() {
   // -=, +=, /=, *=, %=, ~/=
   // increment & decrement ++var, var++, --var, var--
+
+  int a = 0;
+  int b = a++;
+  print(b); // b = 0
+
+  int c = 0;
+  int d = ++c;
+  print(d);
 }
