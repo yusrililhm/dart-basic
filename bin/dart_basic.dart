bcb2de946b5f68f27a50568e68d9b536c058bf85
yusrililhm <yusrililham62@gmail.com>
2023-03-07 16:37:34 +0700

    program hello world di dart

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,5 +1,3 @@
-import 'package:dart_basic/dart_basic.dart' as dart_basic;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_basic.calculate()}!');
+void main() {
+  print('Hello world');
 }
