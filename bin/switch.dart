335a08d83ffea1d00e56296dce40261124cb3ca8
yusrililhm <yusrililham62@gmail.com>
2023-03-08 04:03:18 +0700

    belajar switch di dart

---
+++ bin/switch.dart
@@ -0,0 +1,15 @@
+void main() {
+  String name = 'laras';
+
+  switch (name) {
+    case 'Yurika':
+      print('halo yurika');
+      break;
+    case 'Larasati':
+      print('halo laras');
+      break;
+    default:
+      print('kamu siapa?');
+      break;
+  }
+}
