f2f05d3b93d26fc256cec1121b79b5010280261b
yusrililhm <yusrililham62@gmail.com>
2023-03-08 08:50:35 +0700

    belajar break and continue di dart

---
+++ bin/break_continue.dart
@@ -0,0 +1,15 @@
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    if (i == 5) {
+      break;
+    }
+    print(i);
+  }
+
+  for (int i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+      continue;
+    }
+    print(i);
+  }
+}
